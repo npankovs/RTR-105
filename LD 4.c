4ab852c16888a8a4da875c54f64974dda77b4c7c
npankovs <71449035+npankovs@users.noreply.github.com>
2021-01-22 20:45:22 +0200

    Create LD 4.c

---
+++ LD 4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+void main () {
+float a=0,b=M_PI/2,eps=1.e-3,h, integr1=0,integr2;
+int k,n=2;
+
+integr2=(b-a)*(cos(a)+cos(b))/n;//(b−a)/2. *cos(a)+(b−a)/2.*cos(b);
+while (fabs(integr2-integr1)>eps ){
+n*=2;
+h=(b-a)/n;
+integr1=integr2;
+integr2=0;
+
+for(k=0;k<n; k++)
+integr2+=h*cos(a+(k+0.5)*h);
+}
+printf("Integrala vertība: %.2f\n" , integr2 );
+}
