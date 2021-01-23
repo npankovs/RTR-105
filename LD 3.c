d233205e2166e999c5d45b764f3c05bb67065920
npankovs <71449035+npankovs@users.noreply.github.com>
2021-01-23 15:18:18 +0200

    Update LD 3.c

--- LD 3.c
+++ LD 3.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include <math.h>
 
+float a, b, y, x,delta_x, der1xAN, der1xFD, der2xAN,der2xFD;
+FILE *fp = NULL;
+int main ()
+{
+  printf ("Ievadi sakuma vertibu: ");
+  scanf ("%f", &a);
+    printf ("Ievadi beigu vertibu: ");
+  scanf ("%f", &b);
+    printf ("Ievadi precizitati: ");
+  scanf ("%f", &delta_x);
+  
+  x=a;
+  
+  fp = fopen("derivative.dat" ,"w");
+  fprintf(fp, "  x      y     der1xAN der1xFD der2xAN der2xFD\n");
+  while(x<b){
+    y = cos(x*x);
+    der1xAN =(cos((x+delta_x)*(x+delta_x))-cos( x*x ));
+    der1xFD = -sin(x*x)*2*x;
+    der2xAN =((cos((x+delta_x)*(x+delta_x))-cos( x*x ))+delta_x)-(cos((x+delta_x)*(x+delta_x))-cos( x*x ));
+    der2xFD = -2*sin(x*x)-4*x*x*cos(x*x);
+    fprintf(fp,"%0.2f \t%0.2f \t%0.2f \t%0.2f \t%0.2f \t%0.2f\n",x,y,der1xAN,der1xFD,der2xAN,der2xFD);
+    x += delta_x;
+ 
+}
+  
+  
+  
+return 0;
+}
+
+
+
+
+
+
+/*#include <stdio.h>
+#include <math.h>
+
 float a, b, y, x,delta_x, der1xAnalytical, der1xFiniteD, der2xAnalytical,der2xFiniteD;
 FILE *fp = NULL;
 int main ()
@@ -31,7 +70,7 @@ int main ()
   
   
 return 0;
-}
+}*/
 
 
 /*#include <stdio.h>
